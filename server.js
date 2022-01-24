37f5c796dd225762c2783489c79daa7199b069c9
youngjin yoon <dudwls143@naver.com>
2022-01-24 19:45:17 +0900

    server 크롤링 완료

--- server.js
+++ server.js
@@ -5,20 +5,57 @@ import dotenv from 'dotenv';
 import express from 'express';
 import subRoutes from './routes/subRoutes.js';
 import userRoutes from './routes/UserRoutes.js';
+import { Builder, By, Key, until } from 'selenium-webdriver';
 
-dotenv.config();
+(async function example() {
+    let driver = await new Builder('./chromedriver').forBrowser('chrome').build();
+    // let option = driver.ChromeOptions();
+    // options.add_experimental_option('excludeSwitches', ['enable-logging']);
+    // let browser = driver.Chrome((options = option));
+    try {
+        await driver.get('https://sugang.konkuk.ac.kr/');
 
-const app = express();
-// 미들웨어 -> 순서대로 수행
-app.use(express.json());
-// app.use('/api/user', userRoutes);
+        await driver.switchTo().frame(driver.findElement(By.id('Main')));
 
-app.post('/click', subRoutes);
+        await driver.findElement(By.id('stdNo')).sendKeys('dudwls143');
+        await driver.findElement(By.id('pwd')).sendKeys('@dudwlsdl12');
+        await driver.findElement(By.className('btn-login')).click();
 
-app.post('/signup', userRoutes);
+        await driver.wait(until.ableToSwitchToFrame(By.id('coreMain')), 10000);
 
-app.post('/login', userRoutes);
+        await driver.findElement(By.id('menu_search')).click();
 
-app.listen(3000, () => {
-    console.log('Example app listening on port 3000!');
-});
+        const click = await driver.wait(until.elementLocated(By.xpath('//*[@id="sForm"]/table/tbody/tr[1]/td[2]/label[2]')));
+        click.click();
+
+        await driver.findElement(By.id('pSustMjCd')).sendKeys('컴퓨터공학과');
+
+        await driver.findElement(By.id('btnSearch')).click();
+
+        await driver.sleep(100000);
+
+        const table = await driver.wait(until.elementLocated(By.xpath('//*[@id="gridLecture"]/tbody'))).then((result) => {
+            return result;
+        });
+        // console.log(await table.getText());
+    } finally {
+        // driver.quit();
+    }
+})();
+
+// dotenv.config();
+
+// const app = express();
+// // 미들웨어 -> 순서대로 수행
+// app.use(express.json());
+// // app.use('/api/user', userRoutes);
+
+// app.post('/click', subRoutes);
+
+// app.post('/signup', userRoutes);
+
+// app.post('/login', userRoutes);
+
+// app.listen(3000, () => {
+//     console.log('Example app listening on port 3000!');
+// });
